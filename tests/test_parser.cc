2837b15c1074a301d84f7d019d2bf266f1c7f768
mertensblackfyre <72759681+mertensblackfyre@users.noreply.github.com.>
2025-11-26 21:05:48 +0300

    Hello

--- tests/test_parser.cc
+++ tests/test_parser.cc
@@ -2,10 +2,7 @@
 #include <gtest/gtest.h>
 
 TEST(Parser, ParserInstructions) {
-  EXPECT_EQ(Parser::parse_instruction("M=D       // RAM[1] = the sum"), "M=D");
-  EXPECT_EQ(Parser::parse_instruction("M=D+1       // Hello   "), "M=D+1");
-  EXPECT_EQ(Parser::parse_instruction("@LOOP      // goto LOOP "), "@LOOP");
-  EXPECT_EQ(Parser::parse_instruction("    0;JMP"), "0;JMP");
-  EXPECT_EQ(Parser::parse_instruction("AM= A+1  // AA"), "AM=A+1");
-  EXPECT_EQ(Parser::parse_instruction(" @R1"), "@R1");
-}
+  EXPECT_EQ(Parser::parse_instruction("push segment 1 // Hello World"),
+            "push segment 1");
+  EXPECT_EQ(Parser::parse_instruction("  pop temp 2 "), "pop temp 2");
+};
